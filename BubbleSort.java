6bbd55cab10beab7565381ea82466d19a7c0abad
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2018-09-21 13:56:08 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,12 +1,20 @@
-public class BubbleSort extends AbstractSortingImpl{
+public class BubbleSort extends AbstractSortingImpl {
     @Override
     public void sort(int[] a) {
-        int maxValueIndex;
-        int replaceIndex = a.length - 1;
-        for (int i = 0; i < a.length; i++) {
-            maxValueIndex = findMaxAtIndex(a, 0, replaceIndex + 1);
-            swapValues(a, maxValueIndex, replaceIndex);
-            replaceIndex--;
+//        int maxValueIndex;
+//        int replaceIndex = a.length - 1;
+//        for (int i = 0; i < a.length; i++) {
+//            maxValueIndex = findMaxAtIndex(a, 0, replaceIndex + 1);
+//            swapValues(a, maxValueIndex, replaceIndex);
+//            replaceIndex--;
+//        }
+        for (int j = a.length - 1; j >= 0; j--) {
+            for (int i = 0; i < j; i++) {
+                if (a[i] > a[i + 1]) {
+                    swapValues(a, i, i + 1);
+                }
+            }
         }
+
     }
 }
